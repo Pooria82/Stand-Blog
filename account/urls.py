8c9984604193f48b84a7782f30e9bf0baf37e6d1
Pooria <pooriamadani82@gmail.com>
2024-09-10 12:41:24 +0330

    Correcting how to address urls and implementations related to authentication

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'account'
 urlpatterns = [
     path('login', views.user_login, name='login'),
     path('logout', views.user_logout, name='logout'),
+    path('register', views.user_register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
@@ -5,7 +6,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def user_login(request):
     if request.user.is_authenticated:
-        return redirect('/')
+        return redirect('home:home')
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -13,10 +14,40 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('/')
+            return redirect('home:home')
     return render(request, 'account/login.html', {})
+
+
+def user_register(request):
+    context = {"errors": []}
+    if request.user.is_authenticated:
+        return redirect('home:home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if password1 != password2:
+            context['errors'].append("Passwords must match")
+            return render(request, "account/register.html", context)
+
+        if User.objects.filter(username=username).exists():  # if User.objects.get(username=username):
+            context['errors'].append("Username already taken")
+            return render(request, "account/register.html", context)
+
+        if User.objects.filter(email=email).exists():
+            context['errors'].append("Email already taken")
+            return render(request, "account/register.html", context)
+
+        user = User.objects.create_user(username, email, password1)
+        login(request, user)
+        return redirect('home:home')
+
+    return render(request, "account/register.html", {})
 
 
 def user_logout(request):
     logout(request)
-    return redirect('/')
+    return redirect('home:home')
